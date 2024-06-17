c0e8872c19644f99ee2d35e92dd4d8a6daed57e4
yantao024 <yantao024@gmail.com>
2024-06-16 23:38:18 -0400

    new push

--- testdb.sql
+++ testdb.sql
@@ -22,3 +22,22 @@ GRANT ALL ON testDB.* TO 'sujaya'@'localhost';
 
 -- Change the authentication method of the user to use mysql_native_password
 ALTER USER 'sujaya'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password0!';
+
+-- Check if username or email already exists
+SELECT COUNT() FROM user WHERE username = 'new_username' OR email = 'new_email';
+-- If the count is 0, insert the new user
+INSERT INTO user (username, email, password, Fname, Lname, createdAt, updatedAt)
+VALUES ('new_username', 'new_email', 'hashed_password', 'First_Name', 'Last_Name', NOW(), NOW());
+
+-- Check if the username and password match
+SELECT COUNT() FROM user WHERE username = 'input_username' AND password = 'input_password';
+
+-- Retrieve viewing history for a specific user
+SELECT Title, watchedAt, rating FROM ViewingHistory
+JOIN Title ON ViewingHistory.titleId = Title.titleId
+WHERE ViewingHistory.userId = 'user_id';
+
+-- Search for titles matching a query
+SELECT titleId, primaryTitle FROM Title
+WHERE primaryTitle LIKE '%search_query%';
+
